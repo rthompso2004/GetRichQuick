e62e2a11b7c58b75784dbf7aab3103c0b1a5465b
Ryan Thompson <rthompso2004@gmail.com>
2023-08-10 06:20:20 -0400

    s

---
+++ Exercise1/src/Exercise01.java
@@ -0,0 +1,33 @@
+
+/*
+ * Student Name: Ryan Thompson
+ * Lab Professor: Professor Matthew Neadow
+ * Due Date: Jan 22, 2023.
+ * Modified: Jan 13, 2023.
+ * Description: Exercise01
+ */
+
+/* Description: this class outputs a castle from method main */
+public class Exercise01 {
+
+	// Entry point for the application
+	
+	public static void main(String[] args) {//start method main//
+		
+		System.out.println("  >>>              >>>  ");//output caslte//
+		System.out.println("  |                |    ");
+		System.out.println("= == =           = == = ");
+		System.out.println("|    |     >>>   |    | ");
+		System.out.println("| [] |     |     | [] | ");
+		System.out.println("|    |= = = = = =|    | ");
+		System.out.println("| [] | []     [] | [] | ");
+		System.out.println("|    |    ===    |    | ");
+		System.out.println("| [] |   |   |   | [] | ");
+		System.out.println("|    |   |   |   |    | ");
+		System.out.println("~~~~~~~~~/   /~~~~~~~~~~");
+		System.out.println("~~~~~~~~/   /~~~~~~~~~  ");
+		System.out.println("       /   /            ");
+		System.out.println("Program by Ryan Thompson");//output name//
+		
+	}//end of method main//
+}//end of Exercise01 class//
